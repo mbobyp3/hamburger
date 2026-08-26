c0045e70a9a22d4965b8ecb35b7eae229ed33da6
mbobypratama <mbobyp3@gmail.com>
2026-08-26 22:43:40 +0700

    test: try out js data types

---
+++ learnings/data-types/testing-dataTypes.js
@@ -0,0 +1,23 @@
+let age = 5;
+let city = "Jakarta";
+let hasMarried = true;
+let address = null;
+let name = undefined;
+let str = "123";
+let num = 123;
+let result = 8 + "2"
+let hari = "jum'at"
+let text = age + " years old";
+
+console.log(text);
+console.log(typeof age);
+console.log(typeof city);
+console.log(typeof hasMarried);
+console.log(typeof address);
+console.log(typeof name);
+console.log(typeof str);
+console.log(typeof num);
+console.log(typeof result);
+console.log(result);
+console.log(hari);
+console.log("He said,\"Hello World\"!")
